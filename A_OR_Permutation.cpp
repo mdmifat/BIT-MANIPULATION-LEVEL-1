948adf4bb10d93b0583b56beec3653d48495b020
mdmifat <mifat242-35-184@diu.edu.bd>
2025-07-04 16:39:22 +0600

    Add files via upload

---
+++ A_OR_Permutation.cpp
@@ -0,0 +1,35 @@
+/*
+  In the name of Allah, The Most Gracious and The Most Merciful.
+ 
+  Glory be to You(Allah)! We have no knowledge except what You have taught us.
+  It is you who are the Knowledgeable, the Wise.
+*/
+
+#include<bits/stdc++.h>
+#include<bit>
+#define mod 1000000007
+#define int long long int
+using namespace std;
+const int N = 2e5+10;
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+      int t;
+      cin>>t;
+
+      while(t--){
+          int n;
+          cin>>n;
+
+          for(int i=n;i>=1;i--){
+            cout<<i<<" ";
+          }
+          cout<<endl;
+      }
+    
+    return 0;
+}
